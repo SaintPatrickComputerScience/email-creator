ec66230c40cfeb1073f94dde7bfed8f1ba864aa2
ShamrockProgramming <efleming@stpatrick.org>
2016-08-21 14:56:19 -0500

    fixed problem with the year output.

--- emailCreator.js
+++ emailCreator.js
@@ -1,4 +1,4 @@
 function email(first, last, year){
-  var emailAddress = String(year - 2018)+first[0]+last+"@stpatrick.org";
+  var emailAddress = String(year % 10)+first[0]+last+"@stpatrick.org";
   return emailAddress;
 }
