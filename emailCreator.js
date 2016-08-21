d015882dddcead7bbdde78c98e5ec9efc5210bbb
ShamrockProgramming <efleming@stpatrick.org>
2016-08-21 15:26:43 -0500

    updated code to satisfy additional challenges.

--- emailCreator.js
+++ emailCreator.js
@@ -1,4 +1,39 @@
 function email(first, last, year){
-  var emailAddress = String(year % 10)+first[0]+last+"@stpatrick.org";
+  var emailAddress = graduationYear(year)+first[0]+last+"@stpatrick.org";
   return emailAddress;
+}
+
+function teacherEmail(first, last){
+  var emailAddress = first[0]+last+"@stpatrick.org";
+  return emailAddress;
+}
+
+function graduationYear(grade){
+
+  if(typeof(grade)== "number"){
+    return String(year % 10);
+  }
+  
+  else if(typeof(grade) == "string"){
+      
+    var thisYear = new Date();
+    thisYear = thisYear.getUTCFullYear();
+  
+    if(grade == "freshman"){
+      return (thisYear + 4) % 10;
+    }
+    if(grade == "sophomore"){
+      return (thisYear + 3) % 10;
+    }
+    if(grade == "junior"){
+      return (thisYear + 2) % 10;
+    }
+    if(grade == "senior"){
+      return (thisYear + 1) % 10;
+    }
+  }
+  else{
+    return undefined;
+  }
+  
 }
